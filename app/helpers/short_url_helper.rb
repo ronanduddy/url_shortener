9218bbf1ff2ca34701b44ae455c6c78e50fe9d05
ronanduddy <ronanduddy@live.ie>
2021-01-24 11:53:21 +0000

    Fix double increment issue when viewing a short URL
    The original `link_to` was causing an interesting
    side effect where after clicking the link, two
    requests would be sent to ShortURLConroller#show
    resulting in two views for what should be one.

--- app/helpers/short_url_helper.rb
+++ app/helpers/short_url_helper.rb
@@ -1,6 +1,6 @@
 module ShortUrlHelper
   def vanity_link(short_url)
     slug = short_url.slug
-    link_to vanity_url(slug), vanity_path(slug)
+    link_to(vanity_url(slug), vanity_path(slug), method: :get)
   end
 end
--- spec/helpers/short_url_helper_spec.rb
+++ spec/helpers/short_url_helper_spec.rb
@@ -4,6 +4,10 @@ RSpec.describe ShortUrlHelper do
   describe '#vanity_link' do
     let(:short_url) { FactoryBot.build_stubbed(:short_url, slug: '1234567') }
 
-    it { expect(vanity_link(short_url)).to eq "<a href=\"/1234567\">http://test.host/1234567</a>" }
+    it 'produces the correct link' do
+      expect(vanity_link(short_url)).to eq(
+        "<a data-method=\"get\" href=\"/1234567\">http://test.host/1234567</a>"
+      )
+    end
   end
 end

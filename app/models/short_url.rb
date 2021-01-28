c3ebfc196ebd76b798466d30fc5276174b39c548
ronanduddy <ronanduddy@live.ie>
2021-01-28 13:05:44 +0000

    Update ShortURL to use the UrlValidator
    Also clean up validates statements for readabiltiy

--- app/models/short_url.rb
+++ app/models/short_url.rb
@@ -2,10 +2,8 @@ class ShortUrl < ApplicationRecord
   # TODO: add index to slug
   belongs_to :user
 
-  validates :url, :slug, presence: true
-  validates :url, length: { in: 16..128 }
-  validate  :valid_url
-  validates :slug, length: { is: 7 }, uniqueness: true
+  validates :url, presence: true, length: { in: 16..128 }, url: true
+  validates :slug, presence: true, length: { is: 7 }, uniqueness: true
   validates :views, numericality: { greater_than_or_equal_to: 0 }
 
   before_validation :generate_slug
@@ -20,14 +18,5 @@ class ShortUrl < ApplicationRecord
 
   def generate_slug
     self.slug = SecureRandom.urlsafe_base64[0..6] if self.slug.blank?
-  end
-
-  # TODO: extract into own validator
-  def valid_url
-    url = URI.parse(self.url) rescue false
-
-    unless url.kind_of?(URI::HTTP) || url.kind_of?(URI::HTTPS)
-      errors.add(:url, 'must be valid')
-    end
   end
 end
